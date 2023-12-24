1508523d460a23562141d65bb2eaca01703a8d7d
Okiki Areo <okikiareo@gmail.com>
2023-12-24 20:27:48 +0100

    push

---
+++ src/components/Moviebox.js
@@ -0,0 +1,18 @@
+
+     import {useEffect, useState} from "react";
+
+     function Moviebox (){
+        const[state, setState]=useState([])
+        
+        useEffect(()=> {
+              
+         fetch('https://api.themoviedb.org/3/discover/movie?api_key=d0f00c652a0ec5b927f52935e6ac4e46&sort_by=popularity.desc&language=en-US&page=1').then(data => data.json()).then(movies => {console.log(movies); setState(movies)})  
+      },  []) 
+            return(
+                 <div>
+                     <img src={state.backdrop_path}/>
+                     <div>{state.title}</div>
+               </div>
+            )
+      }    
+     export default Moviebox
