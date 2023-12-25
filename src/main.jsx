2f5fc3e26e3f686df61d9efeabc53dea3a512d17
Okiki Areo <okikiareo@gmail.com>
2023-12-25 20:03:13 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import "./css/main.css";
+import App from "./App";
+
+const container = document.getElementById("root");
+createRoot(container).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
---
+++ src/pages/Home.js
@@ -0,0 +1,74 @@
+// import { Link } from "react-router-dom";
+// import { useEffect } from "react";
+import {useEffect, useState} from "react";
+import tv from "./images/tv.png";
+import menu from "./images/Menu.png";
+import apple from "./images/apple.png";
+import imdb from "./images/imdb.png";
+import search from "./images/search.svg"
+import play from "./images/play.png"
+import React from 'react';
+import Api from "./Api";
+import Moviebox from "./Moviebox";
+
+
+const Home = ({})=>{
+    const [searchKey, setSearchKey] = useState("");
+
+const searchMovie = (e) =>{
+    e.preventDefault()
+    
+}
+
+    return(
+        <>
+<div className="home">
+    <div className="inner">
+       <div className="header flex">
+        <div className="logo flex">
+           <img src={tv} alt=""/> <h2>MovieBox</h2>
+        </div>
+    <form onSubmit={searchMovie}> 
+       <div className="search flex">
+         <input  type="text" onChange={(e) => setSearchKey(e.target.value)} id="input1" placeholder="What do you want to watch?"/>
+         <button type={"submit"}>
+             <img src={search}/>
+             </button> 
+         </div>
+        </form>  
+ {/* {searchKey} */}
+          <div className="sign flex">
+        <h3>Sign in</h3>   <img src={menu}/>
+          </div>
+      </div>
+      <div className="search mobile flex">
+         <input  type="text" id="input1" placeholder="What do you want to watch?"/>
+          <img src={search}/>
+         </div>
+       <div className="header-info">
+       <h3>John Wick 3: <br></br> Parabellum</h3>
+       <div className="two-icons flex">
+       <div className="idm-icon">
+       <img src={imdb}/> 86.0/100
+       </div>
+       <div className="apple-icon">
+       <img src={apple}/> 97%
+       </div>
+       </div >
+       <div className="info">
+       John Wick is on the run after killing a member of the international 
+       assassins' guild, and with a $14 million price tag
+        on his head, he is the target of hit men and women everywhere.
+       </div>
+       <a className="play" to="">
+       <img src={play}/> WATCH TRAILER
+       </a>
+       </div>
+    </div>
+</div>
+<Api />
+<Moviebox />
+</>
+    )
+}
+export default Home;
